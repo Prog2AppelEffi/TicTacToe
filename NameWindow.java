1f0b816fe369182c75d6e3fd6b83d727bb82d0ce
effner <b.effner@tu-braunschweig.de>
2016-06-16 15:06:50 +0200

    Introduction und so geändert

--- NameWindow.java
+++ NameWindow.java
@@ -9,7 +9,7 @@ public class NameWindow extends JFrame implements ActionListener{
 							"Welcome to Tic Tac Toe.<BR>" +
 							"Enter your names and start the game." +
 						  "</center></body></html>";
-	private JLabel introducion = new JLabel(text, JLabel.CENTER);
+	private JLabel introduction = new JLabel(text, JLabel.CENTER);
 	private JTextField inputName1 = new JTextField(1);
 	private JTextField inputName2 = new JTextField(1);
 	private JButton startButton = new JButton("start game");
@@ -19,22 +19,23 @@ public class NameWindow extends JFrame implements ActionListener{
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false); 
         setSize(400, 300);
-        setVisible(true);
+        
         setLocationRelativeTo(null);
         
         JPanel container = new JPanel(new GridLayout(4,1,10,10));
-        introducion:setFont(new Font("",0,15));
+        introduction.setFont(new Font("",0,15));
         inputName1.setFont(new Font("",0,30));
         inputName2.setFont(new Font("",0,30));
         startButton.setFont(new Font("",0,20));
         container.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
-        container.add(introducion);
+        container.add(introduction);
         container.add(inputName1);
         container.add(inputName2);
         container.add(startButton);
         add(container);
 
         startButton.addActionListener(this);
+        setVisible(true);
 	}
 
 	public void actionPerformed(ActionEvent e){
